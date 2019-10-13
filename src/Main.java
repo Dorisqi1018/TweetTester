efaf5a90296a8169ddbad178f6f1137b97eec64f
Wei Qi <45273043+Dorisqi1018@users.noreply.github.com>
2019-10-13 12:52:50 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-	
         Scanner  in = new Scanner(System.in);
         System.out.println("Please enter a tweet: ");
-        String tweet =in.nextLine();
+        String tweet = in.nextLine();
 
         int hashtags = 0, attributions = 0, links = 0;
 
         if (tweet.length() <= 140){
             System.out.println("Length Correct.");
-            for(int i=0; i<tweet.length(); i++){
+            for(int i = 0; i < tweet.length(); i++){
                 char letter = tweet.charAt(i);
 		if (letter == '#')hashtags++;
 		else if (letter == '@')attributions++;
@@ -23,7 +21,7 @@ public class Main {
             System.out.println("Numbers of Attributions: " + attributions);
             System.out.println("Numbers of Links: " + links);
 
-        }else{
+        } else {
             System.out.println("Excess character: " + (tweet.length()-140));
         }
 }
